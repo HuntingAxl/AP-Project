53758ff4acd556d5b13c5050bd99549f48f12c7a
HuntingAxl <gurpal15034@iiitd.ac.in>
2016-11-15 23:22:08 +0530

    Adding a new program named Run.java to create some inner parts of the parser.

---
+++ trying implementation/Run.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Run {
+	public static void main(String[] args) {
+		String s = new String("<title>Pattern Matching in Trees and Nets.</title>");
+		String arr[] = s.split("<");
+		for(String a : arr)
+			System.out.println(a);
+		ArrayList<String> arr1= new ArrayList<String>();
+	}
+
+}
